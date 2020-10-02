72d7a88c2030c2807ae717d07e9b856ab4b527c1
J. Mauricio Benitez <microsistemasprogramados@gmail.com>
2020-10-02 16:43:00 -0300

    Initial Project commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,30 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-<<<<<<< HEAD
 import { ProductoController } from './producto/producto.controller';
 import { ProductoService } from './producto/producto.service';
 import { CarritoController } from './carrito/carrito.controller';
 import { CarritoService } from './carrito/carrito.service';
-=======
->>>>>>> bb39eb5... Preparacion de archivos finalizada, empezar a codear a partir de aca.
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-<<<<<<< HEAD
       rootPath: join(__dirname, '..', 'client'),
       }),
   ],
   controllers: [AppController,CarritoController,ProductoController],
   providers: [AppService,CarritoService,ProductoService],
-=======
-      rootPath: join(__dirname, '..',
-      'client'),
-      }),    
-  ],
-  controllers: [AppController],
-  providers: [AppService],
->>>>>>> bb39eb5... Preparacion de archivos finalizada, empezar a codear a partir de aca.
 })
 export class AppModule {}
