6f8b6beec9fb52d160a36cf1dff0ada3aabfe73c
LucasLastra <lastralucas96@gmail.com>
2020-10-23 18:18:40 -0300

    Agregado menu desplegable para celular

---
+++ client/js/navBar.js
@@ -0,0 +1,8 @@
+function navBar(){
+    let nav = document.getElementById("nav-completo");
+    if(nav.style.display === 'none'){
+        nav.style.display = 'block';
+    }else{
+        nav.style.display = 'none'
+    }
+}
